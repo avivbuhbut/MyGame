0d51cb3ffeb6b4f0473e1375457d8a1a59a705d0
avivbuhbut <avivb931@gmail.com>
2020-10-23 21:54:18 +0300

    23.10.20 21:54

---
+++ MyGame/Assets/Scripts/WeaponAimNShoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAimNShoot : MonoBehaviour
+{
+
+
+    public float bulletVelocity = 5f;
+
+    public GameObject bullet1;
+    Camera myCamera;
+    // Use this for initialization
+    void Start()
+    {
+
+        GameObject cameraObject = GameObject.Find("Camera1"); // change for the name of the tag of the camera
+        myCamera = cameraObject.GetComponent<Camera>();
+        Vector2 mousePosition =
+                        new Vector2(myCamera.ScreenToWorldPoint(Input.mousePosition).x,
+                        myCamera.ScreenToWorldPoint(Input.mousePosition).y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 worldMousePos = myCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (Vector2)((worldMousePos - transform.position));
+            direction.Normalize();
+            // Creates the bullet locally
+            GameObject bullet = (GameObject)Instantiate(
+                                    bullet1,
+                                    transform.position + (Vector3)(direction * 0.5f),
+                                    Quaternion.identity);
+            // Adds velocity to the bullet
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletVelocity;
+        }
+    }
+}
---
+++ MyGame/Assets/Scripts/fire.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fire : MonoBehaviour
+{
+
+
+    GameObject other;
+    GameObject EnemieNew;
+    GameObject heavyBullet1;
+
+    public float firespeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EnemieNew = GameObject.Find("EnemieNew");
+        heavyBullet1 = GameObject.Find("Heavy_Bullet1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * firespeed * Time.deltaTime, Space.Self);
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("HITTT");
+        if (other.name == "EnemieNew")
+        {
+            
+            Destroy(EnemieNew, 2f);
+            Destroy(heavyBullet1, 2f);
+            // playerCollides with the Enemy
+
+        }
+    }
+
+
+
+}
